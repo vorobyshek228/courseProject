d7fe3dcc3f408c7c4b80d036993ae8a5e46e901f
Andrey Kalenik <kalenik.andrey@inbox.ru>
2023-01-04 19:15:38 +0300

    add basket modal

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const btnBasket = document.querySelector("#btnBasket");
+const modalBasket= document.querySelector('#modalBasket');
+
+btnBasket.addEventListener('click', function(){
+    modalBasket.showModal();
+})
