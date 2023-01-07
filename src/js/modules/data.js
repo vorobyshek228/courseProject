317d0b5efeda0ddd5fb8eb1c4b1450bb8f6a3866
Andrey Kalenik <kalenik.andrey@inbox.ru>
2023-01-07 21:11:27 +0300

    add basketGoodsCounter

---
+++ src/js/modules/data.js
@@ -0,0 +1,107 @@
+export let goodsArr = [
+    {
+        id:1,
+        name:"Шорты",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"12",
+        diskont: 10,
+    },
+    {
+        id:2,
+        name:"Шорты",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"14",
+        diskont: 20,
+    
+    },
+    {
+        id:3,
+        name:"Шорты",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"15",
+        diskont: 50,
+    
+    },
+    {
+        id:4,
+        name:"Шорты",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"20",
+        diskont: 10,
+    
+    },
+    {
+        id:5,
+        name:"Майка",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"12",
+        diskont: 10,
+    },
+    {
+        id:6,
+        name:"Майка",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"14",
+        diskont: 20,
+        
+    },
+    {
+        id:7,
+        name:"Майка",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"15",
+        diskont: 50,
+        
+    },
+    {
+        id:8,
+        name:"Майка",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"20",
+        diskont: 10,
+    
+    },
+    {
+        id:9,
+        name:"Штаны",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"12",
+        diskont: 10,
+    },
+    {
+        id:10,
+        name:"Штаны",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"14",
+        diskont: 20,
+        
+    },
+    {
+        id:11,
+        name:"Штаны",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"15",
+        diskont: 50,
+        
+    },
+    {
+        id:12,
+        name:"Штаны",
+        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
+        category:"man",
+        price:"20",
+        diskont: 10,
+    
+    }    
+]
--- src/js/script.js
+++ src/js/script.js
@@ -1,115 +1,12 @@
 import {btnBasket, modalBasket, btnClearBasket, totalPriceValue, shoppingList} from './modules/basket.js'; 
+import {goodsArr} from './modules/data.js';
 
-let goodsArr = [
-    {
-        id:1,
-        name:"Шорты",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"12",
-        diskont: 10,
-    },
-    {
-        id:2,
-        name:"Шорты",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"14",
-        diskont: 20,
-    
-    },
-    {
-        id:3,
-        name:"Шорты",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"15",
-        diskont: 50,
-    
-    },
-    {
-        id:4,
-        name:"Шорты",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"20",
-        diskont: 10,
-    
-    },
-    {
-        id:5,
-        name:"Майка",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"12",
-        diskont: 10,
-    },
-    {
-        id:6,
-        name:"Майка",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"14",
-        diskont: 20,
-        
-    },
-    {
-        id:7,
-        name:"Майка",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"15",
-        diskont: 50,
-        
-    },
-    {
-        id:8,
-        name:"Майка",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"20",
-        diskont: 10,
-    
-    },
-    {
-        id:9,
-        name:"Штаны",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"12",
-        diskont: 10,
-    },
-    {
-        id:10,
-        name:"Штаны",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"14",
-        diskont: 20,
-        
-    },
-    {
-        id:11,
-        name:"Штаны",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"15",
-        diskont: 50,
-        
-    },
-    {
-        id:12,
-        name:"Штаны",
-        imgUrl:"https://basket-01.wb.ru/vol28/part2826/2826015/images/big/1.webp",
-        category:"man",
-        price:"20",
-        diskont: 10,
-    
-    }    
-]
+let topGodsArr =[];
 let basketArr = [];
 let searchArr= [];
 const goodsAera = document.querySelector('.goods-aera');
+const basketGoodsCounter = document.querySelector('#basketGoodsCounter');
+    basketGoodsCounter.style.display= "none";
 
 
 const renderCard = function (item) {
@@ -147,6 +44,8 @@ const renderCard = function (item) {
             const shoppingList = document.querySelector("#shopping-list");
             const totalPrice = document.querySelector("#total-price");
 
+            basketGoodsCounter.style.display = "block";
+            basketGoodsCounter.textContent = basketArr.length;
             shoppingList.innerHTML= "";
             totalPrice.innerHTML= "";
             basketArr.forEach(item => {
@@ -188,7 +87,8 @@ btnClearBasket.addEventListener("click", function(){
     shoppingList.innerHTML = "";
     totalPriceValue.innerHTML = `<strong>Итого: <span class="total"></span>0 р.</strong>`;
     basketArr.length = 0;
-    console.log(basketArr);
+    basketGoodsCounter.textContent = 0;
+    basketGoodsCounter.style.display= "none";
 });
 
 const searchForMain = function (){
@@ -212,3 +112,4 @@ inputSearch.addEventListener("keyup", searchForMain);
 
 
 createCardsAera(goodsArr);
+
