6945f1d89a387e0ba9736bfb9432eb7a460bd08f
andreykalenik <kalenik.andrey@inbox.ru>
2023-01-10 17:35:22 +0300

    the last fix

--- src/js/modules/render-cards.js
+++ src/js/modules/render-cards.js
@@ -49,7 +49,6 @@ const renderCard = function (item, basketArr) {
                     }
         
                     clearBasket();
-        
                     renderBasket(basketArr);
                     setLocalData(basketArr);
 
